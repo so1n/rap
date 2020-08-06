7f77bdaee892ddfe001dbf5191cb965ae7305b2c
So1n <qaz6803609@163.com>
2020-08-06 22:57:05 +0800

    Add, build, add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+
+setuptools.setup(
+    name="rap",
+    version="0.1.0",
+    author="so1n",
+    author_email="so1n897046026@example.com",
+    description="rap(par[::-1]) is a simple and fast python async rpc",
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/so1n/rap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
