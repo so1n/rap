b80ea18735b8b1aaf5108ed7c91e6d8d808252e4
So1n <qaz6803609@163.com>
2020-08-06 10:55:57 +0800

    Modify, add exception

---
+++ rap/exceptions.py
@@ -0,0 +1,29 @@
+class ProtocolError(Exception):
+    def __init__(self, message: str = 'Invalid protocol'):
+        super().__init__(message)
+
+
+class FuncNotFoundError(Exception):
+    pass
+
+
+class RPCError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class ServerError(Exception):
+    def __init__(self, message: str = 'Server Error'):
+        super().__init__(message)
+
+
+class RpcRunTimeError(Exception):
+    def __init__(self, parent, message):
+        self.parent = parent
+        self.message = message
+
+        Exception.__init__(self, "{0}: {1}".format(parent, message))
+
+
+class RegisteredError(Exception):
+    pass
