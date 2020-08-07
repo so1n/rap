f02c5512be0360c59d9335928597be621164e774
So1n <qaz6803609@163.com>
2020-08-08 00:41:36 +0800

    Add, module, aes

---
+++ rap/aes.py
@@ -0,0 +1,37 @@
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+
+
+class Crypto(object):
+    def __init__(self, key: str):
+        self._length = 16
+        self._key = key
+        self._mode = AES.MODE_CBC
+
+    def encrypt(self, raw_data: str) -> bytes:
+        new_crypto: 'AES.new' = AES.new(self._key, self._mode, self._key)
+        count = len(raw_data)
+        if count % self._length != 0:
+            salt = self._length - (count % self._length)
+        else:
+            salt = 0
+        raw_data = raw_data + ('\0' * salt)
+        encrypt_str: str = new_crypto.encrypt(raw_data)
+        return b2a_hex(encrypt_str)
+
+    def decrypt(self, raw_byte: bytes) -> str:
+        new_crypto: 'AES.new' = AES.new(self._key, self._mode, self._key)
+        decrypt_pt: str = new_crypto.decrypt(a2b_hex(raw_byte)).decode()
+        return decrypt_pt.rstrip('\0')
+
+
+if __name__ == '__main__':
+    crypto: 'Crypto' = Crypto('keyskeyskeyskeys')
+    raw_str: str = 'test_rap_crypto_text'
+
+    encrypt_byte: bytes = crypto.encrypt(raw_str)
+    decrypt_str: str = crypto.decrypt(encrypt_byte)
+    print(raw_str)
+    print(encrypt_byte)
+    print(decrypt_str)
+    print(raw_str == decrypt_str)
