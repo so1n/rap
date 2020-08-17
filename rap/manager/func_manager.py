b683c9fcd697d21d01334abea9071e00b966e74c
So1n <qaz6803609@163.com>
2020-08-17 23:03:27 +0800

    Modify, style, modify func name

--- rap/manager/func_manager.py
+++ rap/manager/func_manager.py
@@ -13,9 +13,9 @@ class FuncManager(object):
         self._cwd: str = os.getcwd()
         self.func_dict: Dict[str, Callable] = dict()
         self.generator_dict: dict = {}
-        self.register(self.load, '_root_load')
-        self.register(self.reload, '_root_reload')
-        self.register(self.get_register_func, '_root_list')
+        self.register(self._load, '_root_load')
+        self.register(self._reload, '_root_reload')
+        self.register(self._get_register_func, '_root_list')
 
     def register(self, func: Optional[Callable], name: Optional[str] = None):
         if inspect.isfunction(func) or inspect.ismethod(func):
@@ -29,23 +29,29 @@ class FuncManager(object):
         self.func_dict[name] = func
         logging.info(f"register func:{name}")
 
-    def load(self, path: str, func_str: str) -> str:
-        reload_module = importlib.import_module(path)
-        func = getattr(reload_module, func_str)
-        self.register(func)
-        return f"load {func_str} from {path} success"
+    def _load(self, path: str, func_str: str) -> str:
+        try:
+            reload_module = importlib.import_module(path)
+            func = getattr(reload_module, func_str)
+            self.register(func)
+            return f"load {func_str} from {path} success"
+        except Exception as e:
+            raise RegisteredError(str(e))
 
-    def reload(self, path: str, func_str: str) -> str:
-        if func_str not in self.func_dict:
-            raise RegisteredError(f"func:{func_str} not found")
-        reload_module = importlib.import_module(path)
-        func = getattr(reload_module, func_str)
-        if not hasattr(func, "__call__"):
-            raise RegisteredError(f"{func_str} is not a callable object")
-        self.func_dict[func_str] = func
-        return f"reload {func_str} from {path} success"
+    def _reload(self, path: str, func_str: str) -> str:
+        try:
+            if func_str not in self.func_dict:
+                raise RegisteredError(f"func:{func_str} not found")
+            reload_module = importlib.import_module(path)
+            func = getattr(reload_module, func_str)
+            if not hasattr(func, "__call__"):
+                raise RegisteredError(f"{func_str} is not a callable object")
+            self.func_dict[func_str] = func
+            return f"reload {func_str} from {path} success"
+        except Exception as e:
+            raise RegisteredError(str(e))
 
-    def get_register_func(self) -> List[Tuple[str, str, str]]:
+    def _get_register_func(self) -> List[Tuple[str, str, str]]:
         register_list: List[Tuple[str, str, str]] = []
         for key, value in self.func_dict.items():
             module = inspect.getmodule(value)
