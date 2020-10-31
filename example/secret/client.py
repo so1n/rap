d10a941b4f8255aa095b4fa3ee44f9ea807853dc
So1n <qaz6803609@163.com>
2020-11-01 01:01:35 +0800

    Modify, example, add secret example

---
+++ example/secret/client.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+
+from rap.client import Client
+
+
+client = Client(secret_tuple=('test', 'keyskeyskeyskeys'))  # enable secret
+
+
+# in register, must use async def...
+@client.register
+async def async_sum(a: int, b: int) -> int:
+    pass
+
+
+async def main():
+    await client.connect()
+    print(f"async result: {await async_sum(1, 3)}")
+    await client.wait_close()
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(
+        format='[%(asctime)s %(levelname)s] %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S',
+        level=logging.DEBUG
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ example/secret/server.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from rap.server import Server
+
+
+async def async_sum(a: int, b: int) -> int:
+    await asyncio.sleep(1)  # mock io time
+    return a + b
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(
+        format='[%(asctime)s %(levelname)s] %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S',
+        level=logging.DEBUG
+    )
+
+    loop = asyncio.new_event_loop()
+    rpc_server = Server(
+        secret_dict={'test': 'keyskeyskeyskeys'},  # enable secret
+    )
+    rpc_server.register(async_sum)
+    server = loop.run_until_complete(rpc_server.create_server())
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
