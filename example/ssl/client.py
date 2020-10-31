604e58b1fed46fcce54b2f9c8c11d945042cc036
So1n <qaz6803609@163.com>
2020-11-01 01:01:50 +0800

    Modify, example, add sll example

---
+++ example/ssl/client.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from rap.client import Client
+
+
+client = Client(secret_tuple=('test', 'keyskeyskeyskeys'))  # enable secret
+
+
+# in register, must use async def...
+@client.register
+async def async_sum(a: int, b: int) -> int:
+    pass
+
+
+async def main():
+    """
+    # gen ssl key sh
+    > openssl req -newkey rsa:2048 -nodes -keyout rap_ssl.key -x509 -days 365 -out rap_ssl.crt
+    """
+    await client.connect(ssl_crt_path='./rap_ssl.crt')  # enable ssl
+    print(f"async result: {await async_sum(1, 3)}")
+    await client.wait_close()
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(
+        format='[%(asctime)s %(levelname)s] %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S',
+        level=logging.DEBUG
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
