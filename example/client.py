325b0a6897f9578a378905e87986c26e692cc6d2
So1n <qaz6803609@163.com>
2020-11-07 16:14:46 +0800

    Modify, refactor, remove conn pool and add conn multiplexing

--- example/client.py
+++ example/client.py
@@ -24,13 +24,13 @@ async def async_gen(a: int):
 
 
 async def _run_once():
-    print(f"sync result: {await client.call(sync_sum, 1, 2)}")
-    print(f"reload :{ await client.raw_call('_root_reload', 'test_module', 'sync_sum')}")
-    print(f"sync result: {await client.raw_call('sync_sum', 1, 2)}")
+    # print(f"sync result: {await client.call(sync_sum, 1, 2)}")
+    # print(f"reload :{ await client.raw_call('_root_reload', 'test_module', 'sync_sum')}")
+    # print(f"sync result: {await client.raw_call('sync_sum', 1, 2)}")
 
     print(f"async result: {await async_sum(1, 3)}")
-    async for i in async_gen(10):
-        print(f"async gen result:{i}")
+    # async for i in async_gen(10):
+    #     print(f"async gen result:{i}")
 
 
 async def run_once():
@@ -44,14 +44,8 @@ async def run_once():
 async def conn():
     s_t = time.time()
     await client.connect()
-    await asyncio.wait([_run_once() for i in range(100)])
-    print(time.time() - s_t)
-    await client.wait_close()
-
-
-async def pool():
-    s_t = time.time()
-    await client.create_pool()
+    # for i in range(100):
+    #     await _run_once()
     await asyncio.wait([_run_once() for i in range(100)])
     print(time.time() - s_t)
     await client.wait_close()
@@ -65,4 +59,6 @@ if __name__ == "__main__":
     )
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_once())
+    # loop.run_until_complete(run_once())
+    loop.run_until_complete(conn())
+
