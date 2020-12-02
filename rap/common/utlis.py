c4bc20ffa674945a9876b8650cfcbc16f000dfef
So1n <qaz6803609@163.com>
2020-12-03 00:15:37 +0800

    Modify, refactor server exc & call func ex

--- rap/common/utlis.py
+++ rap/common/utlis.py
@@ -13,7 +13,7 @@ _STR_LD = string.ascii_letters + string.digits
 
 
 class Constant(object):
-    VERSION: str = "0.5.1"
+    VERSION: str = "0.5.2.1"
     USER_AGENT: str = "Python3"
     SOCKET_RECV_SIZE: int = 1024 ** 1
 
