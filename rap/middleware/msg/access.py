722d10d47e125d98e1b16f3128db1df20548620d
So1n <qaz6803609@163.com>
2020-12-04 01:53:35 +0800

    Modify, refactor, msg middleware

--- rap/middleware/msg/access.py
+++ rap/middleware/msg/access.py
@@ -3,24 +3,24 @@ import time
 from typing import Callable, Tuple, Union
 
 from rap.common.exceptions import ServerError
-from rap.manager.client_manager import ClientModel
 from rap.middleware.base_middleware import BaseMsgMiddleware
+from rap.server.requests import RequestModel
 
 
 class AccessMsgMiddleware(BaseMsgMiddleware):
     async def dispatch(
-        self, header: dict, call_id: int, method: Callable, param: str, client_model: "ClientModel"
+        self, request: RequestModel, call_id: int, func: Callable, param: str
     ) -> Tuple[int, Union[dict, Exception]]:
         start_time: float = time.time()
         status: bool = False
 
         try:
-            call_id, result = await self.call_next(header, call_id, method, param, client_model)
+            call_id, result = await self.call_next(request, call_id, func, param)
             status = True
         except Exception as e:
             logging.exception(e)
             result = ServerError("execute func error")
         if isinstance(result, Exception):
             status = False
-        logging.info(f"Method:{method}, time:{time.time() - start_time}, status:{status}")
+        logging.info(f"func:{func}, time:{time.time() - start_time}, status:{status}")
         return call_id, result
