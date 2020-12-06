993874b2c3f15dfb9d3ab9ff5eecec8989506017
So1n <qaz6803609@163.com>
2020-12-07 00:33:05 +0800

    Modify, feat, ip max limit

--- rap/server/middleware/conn/conn_limit.py
+++ rap/server/middleware/conn/conn_limit.py
@@ -3,39 +3,50 @@ from collections import defaultdict
 from typing import Dict
 
 from rap.common.conn import ServerConnection
+from rap.manager.redis_manager import redis_manager
 from rap.server.middleware.base import BaseConnMiddleware
 from rap.server.response import ResponseModel, Response
 
 
 class ConnLimitMiddleware(BaseConnMiddleware):
-    def __init__(self, max_conn: int = 1024, ip_max_conn: int = 128):
+    def __init__(self, max_conn: int = 1024):
         self._max_conn: int = max_conn
-        self._ip_max_conn: int = ip_max_conn
         self._conn_count: int = 0
-        self._ip_count_dict: Dict[str, int] = defaultdict(lambda: 0)
 
     async def dispatch(self, conn: ServerConnection):
-        self._conn_count += 1
-        ip: str = conn.peer[0]
-        self._ip_count_dict[ip] += 1
-        try:
-            if self._conn_count > self._max_conn:
-                logging.error(f"Currently exceeding the maximum number of connections limit, close {conn.peer}")
-                await Response()(
-                    conn,
-                    ResponseModel(event=("close conn", "Currently exceeding the maximum number of connections limit")),
-                )
-                await conn.await_close()
-            if self._ip_count_dict[ip] > self._ip_max_conn:
-                logging.error(f"Currently exceeding the maximum number of ip conn limit, close {conn.peer}")
-                await Response()(
-                    conn,
-                    ResponseModel(event=("close conn", "Currently exceeding the maximum number of ip conn limit")),
-                )
-                await conn.await_close()
-            else:
-                logging.info(f"new conn:{conn.peer}")
+        if self._conn_count > self._max_conn:
+            logging.error(f"Currently exceeding the maximum number of connections limit, close {conn.peer}")
+            await Response()(
+                conn,
+                ResponseModel(event=("close conn", "Currently exceeding the maximum number of connections limit")),
+            )
+            await conn.await_close()
+        else:
+            try:
+                self._conn_count += 1
                 await self.call_next(conn)
-        finally:
-            self._conn_count -= 1
-            self._ip_count_dict[ip] -= 1
+            finally:
+                self._conn_count -= 1
+
+
+class IpMaxConnMiddleware(BaseConnMiddleware):
+    def __init__(self, ip_max_conn: int = 128, timeout: int = 180):
+        self._ip_max_conn: int = ip_max_conn
+        self._timeout: int = timeout
+
+    async def dispatch(self, conn: ServerConnection):
+        key: str = redis_manager.namespace + conn.peer[0]
+        if (await redis_manager.redis_pool.get(key)) > self._ip_max_conn:
+            logging.error(f"Currently exceeding the maximum number of ip conn limit, close {conn.peer}")
+            await Response()(
+                conn,
+                ResponseModel(event=("close conn", "Currently exceeding the maximum number of ip conn limit")),
+            )
+            await conn.await_close()
+        else:
+            await redis_manager.redis_pool.incr(key)
+            try:
+                await self.call_next(conn)
+            finally:
+                await redis_manager.redis_pool.decr(key)
+                await redis_manager.redis_pool.expire(key, self._timeout)
