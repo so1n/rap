45be556df6803b0f7429b3dfd9ab20100c6df127
So1n <qaz6803609@163.com>
2020-12-06 16:14:47 +0800

    Modify, feat, add lp max limit

--- rap/server/middleware/conn/conn_limit.py
+++ rap/server/middleware/conn/conn_limit.py
@@ -1,17 +1,23 @@
 import logging
+from collections import defaultdict
+from typing import Dict
 
 from rap.common.conn import ServerConnection
 from rap.server.middleware.base import BaseConnMiddleware
 from rap.server.response import ResponseModel, Response
 
 
-class AccessConnMiddleware(BaseConnMiddleware):
-    def __init__(self, max_conn: int = 1024):
+class ConnLimitMiddleware(BaseConnMiddleware):
+    def __init__(self, max_conn: int = 1024, ip_max_conn: int = 128):
         self._max_conn: int = max_conn
+        self._ip_max_conn: int = ip_max_conn
         self._conn_count: int = 0
+        self._ip_count_dict: Dict[str, int] = defaultdict(lambda: 0)
 
     async def dispatch(self, conn: ServerConnection):
         self._conn_count += 1
+        ip: str = conn.peer[0]
+        self._ip_count_dict[ip] += 1
         try:
             if self._conn_count > self._max_conn:
                 logging.error(f"Currently exceeding the maximum number of connections limit, close {conn.peer}")
@@ -20,8 +26,16 @@ class AccessConnMiddleware(BaseConnMiddleware):
                     ResponseModel(event=("close conn", "Currently exceeding the maximum number of connections limit")),
                 )
                 await conn.await_close()
+            if self._ip_count_dict[ip] > self._ip_max_conn:
+                logging.error(f"Currently exceeding the maximum number of ip conn limit, close {conn.peer}")
+                await Response()(
+                    conn,
+                    ResponseModel(event=("close conn", "Currently exceeding the maximum number of ip conn limit")),
+                )
+                await conn.await_close()
             else:
                 logging.info(f"new conn:{conn.peer}")
                 await self.call_next(conn)
         finally:
             self._conn_count -= 1
+            self._ip_count_dict[ip] -= 1
