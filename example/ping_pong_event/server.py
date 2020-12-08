cc551954135e5d32fcbdcffc8998415bc38db8b7
So1n <qaz6803609@163.com>
2020-12-09 00:40:38 +0800

    Modify, example, add ping&pone example

---
+++ example/ping_pong_event/server.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from rap.server import Server
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(
+        format="[%(asctime)s %(levelname)s] %(message)s", datefmt="%y-%m-%d %H:%M:%S", level=logging.DEBUG
+    )
+
+    loop = asyncio.new_event_loop()
+    rpc_server = Server()
+    loop.run_until_complete(rpc_server.create_server())
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(rpc_server.wait_closed())
