be3de041f2b7c001070d4d866dd57b78cc4c8348
So1n <qaz6803609@163.com>
2020-12-09 00:40:38 +0800

    Modify, example, add ping&pone example

---
+++ example/ping_pong_event/client.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from rap.client import Client
+from rap.client.middleware.crypto import CryptoMiddleware
+
+
+client = Client()
+
+
+async def main():
+    await client.connect()
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(
+        format="[%(asctime)s %(levelname)s] %(message)s", datefmt="%y-%m-%d %H:%M:%S", level=logging.DEBUG
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.run_forever()
