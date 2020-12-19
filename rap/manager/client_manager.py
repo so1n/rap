515242e9e8c27e3e03432953b6c8a11a7f775d1d
So1n <qaz6803609@163.com>
2020-12-20 01:43:09 +0800

    Modify, refactor, request

--- rap/manager/client_manager.py
+++ rap/manager/client_manager.py
@@ -2,36 +2,17 @@ import asyncio
 import logging
 import time
 from dataclasses import dataclass, field
-from enum import Enum, auto
 from typing import Dict, Generator, Optional
 
 from rap.common.utlis import gen_random_time_id, MISS_OBJECT
-
-
-class LifeCycleEnum(Enum):
-    declare = auto()
-    msg = auto()
-    drop = auto()
 
 
 @dataclass()
 class ClientModel(object):
     client_id: Optional[str] = None
     generator_dict: Dict[int, Generator] = field(default_factory=dict)
-    _life_cycle: "LifeCycleEnum" = LifeCycleEnum.declare
     keep_alive_timestamp: int = int(time.time())
     ping_event_future: Optional[asyncio.Future] = None
-
-    def modify_life_cycle(self, life_cycle: "LifeCycleEnum") -> bool:
-        now_life_cycle: "LifeCycleEnum" = self._life_cycle
-        result: bool = False
-        if now_life_cycle is LifeCycleEnum.msg and life_cycle is LifeCycleEnum.msg:
-            result = True
-        elif now_life_cycle is LifeCycleEnum.declare and life_cycle is LifeCycleEnum.msg:
-            result = True
-        elif now_life_cycle is not LifeCycleEnum.drop and life_cycle is LifeCycleEnum.drop:
-            result = True
-        return result
 
 
 class ClientManager(object):
