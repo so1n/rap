bb978db147ad4b61525a3191eb172e41ae3feba8
So1n <qaz6803609@163.com>
2021-01-02 00:13:24 +0800

    Modify, doc, modify server middleware&processor doc

--- rap/server/processor/base.py
+++ rap/server/processor/base.py
@@ -2,6 +2,10 @@ from rap.server.model import RequestModel, ResponseModel
 
 
 class BaseProcessor(object):
+    """
+    feat: Process the data of a certain process (usually used to read data and write data)
+    ps: If you need to share data, please use `request.stats` and `response.stats`
+    """
     async def process_request(self, request: RequestModel):
         pass
 
