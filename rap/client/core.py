20faf7dabdec76a00ae20f6581e50dfdd4cc6566
So1n <qaz6803609@163.com>
2021-01-26 00:47:31 +0800

    Modify, feat, client, register support alias & group

--- rap/client/core.py
+++ rap/client/core.py
@@ -7,6 +7,7 @@ from rap.client.processor.base import BaseProcessor
 from rap.client.transoprt.channel import Channel
 from rap.client.transoprt.transport import Session, Transport
 from rap.common.conn import Connection
+from rap.common.utlis import MISS_OBJECT
 
 __all__ = ["Client"]
 
@@ -14,26 +15,41 @@ __all__ = ["Client"]
 class AsyncIteratorCall:
     """let client support async iterator (keep sending and receiving messages under the same conn)"""
 
-    def __init__(self, method: str, client: "Client", *args: Tuple, header: Optional[dict] = None):
+    def __init__(
+            self,
+            method: str,
+            client: "Client",
+            *args: Tuple,
+            header: Optional[dict] = None,
+            group: Optional[str] = None,
+            session: Optional[Session] = None,
+    ):
+        self.group: Optional[str] = group
         self._method: str = method
         self._call_id: Optional[int] = None
         self._args: Tuple = args
         self._client: "Client" = client
         self._header: Optional[dict] = header
 
-        self._session: Session = self._client.transport.session
-        self._in_session: bool = self._session.in_session
+        if session:
+            self._session: Session = session
+        else:
+            self._session = self._client.transport.get_now_session()
+            if self._session is MISS_OBJECT:
+                self._session = self._client.transport.session
+        self.in_session: bool = self._session.in_session
+        print(self.in_session)
 
     ###################
     # session support #
     ###################
     async def __aenter__(self) -> "AsyncIteratorCall":
-        if not self._in_session:
+        if not self.in_session:
             self._session.create()
         return self
 
     async def __aexit__(self, *args: Tuple):
-        if not self._in_session:
+        if not self.in_session:
             self._session.close()
 
     #####################
@@ -49,7 +65,12 @@ class AsyncIteratorCall:
         If no data, the server will return header.status_code = 301 and client must raise StopAsyncIteration Error.
         """
         response: Response = await self._client.transport.request(
-            self._method, *self._args, call_id=self._call_id, header=self._header, session=self._session
+            self._method,
+            *self._args,
+            call_id=self._call_id,
+            header=self._header,
+            session=self._session,
+            group=self.group
         )
         self._call_id = response.body["call_id"]
         if response.header["status_code"] == 301:
@@ -59,11 +80,11 @@ class AsyncIteratorCall:
 
 class Client:
     def __init__(
-        self,
-        host_list: Optional[List[str]] = None,
-        timeout: int = 9,
-        keep_alive_time: int = 1200,
-        ssl_crt_path: Optional[str] = None,
+            self,
+            host_list: Optional[List[str]] = None,
+            timeout: int = 9,
+            keep_alive_time: int = 1200,
+            ssl_crt_path: Optional[str] = None,
     ):
         """
         host_list:
@@ -104,31 +125,34 @@ class Client:
     #####################
     # register func api #
     #####################
-    def _async_register(self, func: Callable):
+    def _async_register(self, func: Callable, group: Optional[str], name: Optional[str] = None):
         """Decorate normal function"""
+        name: str = name if name else func.__name__
 
         @wraps(func)
         async def wrapper(*args: Any, **kwargs: Any) -> Any:
-            return await self.raw_call(func.__name__, *args, **kwargs)
+            return await self.raw_call(name, *args, group=group, **kwargs)
 
         return cast(Callable, wrapper)
 
-    def _async_gen_register(self, func: Callable):
+    def _async_gen_register(self, func: Callable, group: Optional[str], name: Optional[str] = None):
         """Decoration generator function"""
+        name: str = name if name else func.__name__
 
         @wraps(func)
         async def wrapper(*args, **kwargs) -> Any:
-            async for result in self.iterator_call(func.__name__, *args, **kwargs):
+            async for result in self.iterator_call(name, *args, group=group, **kwargs):
                 yield result
 
         return cast(Callable, wrapper)
 
-    def _async_channel_register(self, func: Callable):
+    def _async_channel_register(self, func: Callable, name: Optional[str] = None):
         """Decoration channel function"""
+        name: str = name if name else func.__name__
 
         @wraps(func)
         async def wrapper(*args, **kwargs) -> Any:
-            async with self.transport.channel(func.__name__) as channel:
+            async with self.transport.channel(name) as channel:
                 await func(channel)
 
         return cast(Callable, wrapper)
@@ -137,48 +161,92 @@ class Client:
     # client base api #
     ###################
     async def raw_call(
-        self,
-        method: str,
-        *args: Any,
-        conn: Optional[Connection] = None,
-        header: Optional[dict] = None,
-        group: str = "default",
-        session: Optional["Session"] = None,
+            self,
+            method: str,
+            *args: Any,
+            conn: Optional[Connection] = None,
+            header: Optional[dict] = None,
+            group: Optional[str] = None,
+            session: Optional["Session"] = None,
     ) -> Any:
-        """rpc client base call method"""
+        """rpc client base call method
+        method: func name
+        args: python args
+        conn: rap.client conn
+          if conn is None, rap will random choice conn from transport
+          if session is not None, rap.client will not use conn
+        header: request's header
+        group: func's group, default group value is `default`
+        session: conn session
+        """
         response = await self.transport.request(method, *args, conn=conn, group=group, header=header, session=session)
         return response.body["result"]
 
     async def call(
-        self,
-        func: Callable,
-        *args: Any,
-        conn: Optional[Connection] = None,
-        header: Optional[dict] = None,
-        session: Optional["Session"] = None,
+            self,
+            func: Callable,
+            *args: Any,
+            conn: Optional[Connection] = None,
+            header: Optional[dict] = None,
+            group: Optional[str] = None,
+            session: Optional["Session"] = None,
     ) -> Any:
-        """automatically resolve function names and call call_by_text"""
-        return await self.raw_call(func.__name__, *args, conn=conn, header=header, session=session)
+        """automatically resolve function names and call raw_call
+        func: rpc func
+        args: python args
+        conn: rap.client conn
+          if conn is None, rap will random choice conn from transport
+          if session is not None, rap.client will not use conn
+        header: request's header
+        group: func's group, default group value is `default`
+        session: conn session
+        """
+        return await self.raw_call(func.__name__, *args, conn=conn, group=group, header=header, session=session)
 
-    async def iterator_call(self, method: str, *args: Any, header: Optional[dict] = None) -> Any:
-        """Python-specific generator call"""
-        async with AsyncIteratorCall(method, self, *args, header=header) as async_iterator:
+    async def iterator_call(
+            self,
+            method: str,
+            *args: Any,
+            header: Optional[dict] = None,
+            group: Optional[str] = None,
+            session: Optional["Session"] = None
+    ) -> Any:
+        """Python-specific generator call
+        method: func name
+        args: python args
+        header: request's header
+        group: func's group, default group value is `default`
+        session: conn session
+        """
+        async with AsyncIteratorCall(
+                method, self, *args, header=header, group=group, session=session
+        ) as async_iterator:
             async for result in async_iterator:
                 yield result
 
-    def register(self, func: Callable) -> Any:
+    def register(self, name: Optional[str] = None, group: Optional[str] = None) -> Any:
         """Using this method to decorate a fake function can help you use it better.
         (such as ide completion, ide reconstruction and type hints)
-        and will be automatically registered according to the function type"""
-        func_sig: inspect.Signature = inspect.signature(func)
-        func_arg_parameter: List[inspect.Parameter] = [i for i in func_sig.parameters.values() if i.default == i.empty]
-        if len(func_arg_parameter) == 1 and func_arg_parameter[0].annotation is Channel:
-            return self._async_channel_register(func)
+        and will be automatically registered according to the function type
 
-        if inspect.iscoroutinefunction(func):
-            return self._async_register(func)
-        elif inspect.isasyncgenfunction(func):
-            return self._async_gen_register(func)
+        group: func's group, default group value is `default`
+        """
+
+        def wrapper(func: Callable):
+            func_sig: inspect.Signature = inspect.signature(func)
+            func_arg_parameter: List[inspect.Parameter] = [
+                i for i in func_sig.parameters.values() if i.default == i.empty
+            ]
+            if len(func_arg_parameter) == 1 and func_arg_parameter[0].annotation is Channel:
+                if group:
+                    raise RuntimeError("channel func not support group")
+                return self._async_channel_register(func, name=name)
+            if inspect.iscoroutinefunction(func):
+                return self._async_register(func, group, name=name)
+            elif inspect.isasyncgenfunction(func):
+                return self._async_gen_register(func, group, name=name)
+
+        return wrapper
 
     @property
     def session(self) -> "Session":
