496250de3b1d584c5344b943b7450b3ae8d5bfee
So1n <qaz6803609@163.com>
2021-03-02 23:42:11 +0800

    Modify, fix&test, get all allow or black ip bug

--- tests/test_limit.py
+++ tests/test_limit.py
@@ -12,23 +12,21 @@ from .conftest import async_sum
 pytestmark = pytest.mark.asyncio
 
 
-def match_demo_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
-    if request.func_name == "async_sum":
-        return request.func_name
-    else:
-        return None
-
-
-def match_ip_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
-    key: str = "127.0.0.1"
-    if request.header["host"][0] == key:
-        return key
-    else:
-        return None
-
-
 class TestLimit:
     async def test_limit(self, rap_server: Server, rap_client: Client) -> None:
+        def match_demo_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
+            if request.func_name == "async_sum":
+                return request.func_name
+            else:
+                return None
+
+        def match_ip_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
+            key: str = "127.0.0.1"
+            if request.header["host"][0] == key:
+                return key + "1"
+            else:
+                return None
+
         redis: StrictRedis = StrictRedis.from_url("redis://localhost")
         limit_processor = limit.LimitProcessor(
             limit.backend.RedisTokenBucketBackend(redis),
@@ -42,6 +40,44 @@ class TestLimit:
         with pytest.raises(TooManyRequest):
             assert 3 == await async_sum(1, 2)
             assert 3 == await async_sum(1, 2)
+
+    async def test_limit_by_redis_fixed_window_backend(self, rap_server: Server, rap_client: Client) -> None:
+        def match_ip_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
+            key: str = "127.0.0.1"
+            if request.header["host"][0] == key:
+                return key + "2"
+            else:
+                return None
+
+        redis: StrictRedis = StrictRedis.from_url("redis://localhost")
+        limit_processor = limit.LimitProcessor(
+            limit.backend.RedisFixedWindowBackend(redis),
+            [
+                (match_ip_request, limit.Rule(second=5, gen_token=1, init_token=1, max_token=10, block_time=1)),
+            ],
+        )
+        rap_server.load_processor([limit_processor])
+
+        with pytest.raises(TooManyRequest):
+            assert 3 == await rap_client.raw_call('sync_sum', 1, 2)
+            assert 3 == await rap_client.raw_call('sync_sum', 1, 2)
+
+    async def test_limit_by_redis_cell_backend(self, rap_server: Server, rap_client: Client) -> None:
+        def match_ip_request(request: RequestModel) -> limit.RULE_FUNC_RETURN_TYPE:
+            key: str = "127.0.0.1"
+            if request.header["host"][0] == key:
+                return key + "3"
+            else:
+                return None
+
+        redis: StrictRedis = StrictRedis.from_url("redis://localhost")
+        limit_processor = limit.LimitProcessor(
+            limit.backend.RedisCellBackend(redis),
+            [
+                (match_ip_request, limit.Rule(second=5, gen_token=1, init_token=1, max_token=10, block_time=1)),
+            ],
+        )
+        rap_server.load_processor([limit_processor])
 
         with pytest.raises(TooManyRequest):
             assert 3 == await rap_client.raw_call('sync_sum', 1, 2)
