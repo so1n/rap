d7518b1b86b019030b8f37e24b956bd16c65def9
so1n <qaz6803609@163.com>
2021-05-18 01:32:51 +0800

    Modify, example, add api_gateway example

--- example/api_gateway/api_server.py
+++ example/api_gateway/api_server.py
@@ -1,9 +1,8 @@
 import uvicorn  # type: ignore
 from starlette.applications import Starlette
 
-from example.api_gateway.server import rpc_server
+from rap.api_gateway.app import create_app
 from rap.client import Client
-from rap.server.api_gateway import create_app
 
-app: Starlette = create_app("/api", rpc_server, Client())
+app: Starlette = create_app("/api", Client())
 uvicorn.run(app)
