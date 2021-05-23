1b4b4836635345feea1c859509fef29eb9dddef2
so1n <qaz6803609@163.com>
2021-05-23 16:36:06 +0800

    Add, Feature, add statsd processor

---
+++ rap/server/plugin/processor/statsd_processor.py
@@ -0,0 +1,51 @@
+import socket
+from typing import Optional
+
+from aio_statsd import StatsdClient  # type: ignore
+
+from rap.common.utils import Constant
+from rap.server.model import Request, Response
+from rap.server.plugin.processor.base import BaseProcessor
+
+
+class StatsdProcessor(BaseProcessor):
+    """Note: not test..."""
+
+    def __init__(self, statsd_client: StatsdClient, namespace: Optional[str] = None) -> None:
+        self._statsd_client: StatsdClient = statsd_client
+
+        self._namespace: str = namespace or f"rap.client.{socket.gethostname()}"
+
+        self._channel_online_key: str = f"{self._namespace}.channel_online"
+        self._channel_key: str = f"{self._namespace}.channel"
+        self._msg_key: str = f"{self._namespace}.msg"
+        self._request_key: str = f"{self._namespace}.request"
+        self._error_request_key: str = f"{self._namespace}.request.error"
+
+    def start_event_handle(self) -> None:
+        def upload_metric(stats_dict: dict) -> None:
+            for key, values in stats_dict.items():
+                self._statsd_client.counter(key, values)
+
+        if self.app.window_state:
+            self.app.window_state.add_callback(upload_metric)
+
+    async def process_request(self, request: Request) -> Request:
+        self._statsd_client.increment(self._request_key, 1)
+        if request.num == Constant.MSG_REQUEST:
+            self._statsd_client.increment(self._msg_key, 1)
+        host: str = request.header["host"]
+        self._statsd_client.sets(f"{self._namespace}.online.{host}", 1)
+        return request
+
+    async def process_response(self, response: Response) -> Response:
+        if response.num == Constant.CHANNEL_RESPONSE:
+            life_cycle: str = response.header.get("channel_life_cycle", "error")
+            if life_cycle == Constant.DECLARE:
+                self._statsd_client.increment(self._channel_online_key, 1)
+                self._statsd_client.increment(self._channel_key, 1)
+            elif life_cycle == Constant.DROP:
+                self._statsd_client.decrement(self._channel_online_key, 1)
+        elif response.num == Constant.SERVER_ERROR_RESPONSE:
+            self._statsd_client.increment(self._error_request_key, 1)
+        return response
