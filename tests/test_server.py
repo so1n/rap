df6d7dc1eded8f9cc0d5294cb78fa1056fb8abc1
so1n <qaz6803609@163.com>
2021-07-10 02:56:25 +0800

    Modify, Refactor, server context

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,6 +7,7 @@ from pytest_mock import MockerFixture
 from rap.client import Client
 from rap.common.exceptions import RpcRunTimeError, ServerError
 from rap.server import Server
+from rap.server.context import Context
 from rap.server.plugin.middleware.conn.limit import ConnLimitMiddleware
 from rap.server.plugin.processor import CryptoProcessor as ServerCryptoProcessor
 
@@ -116,3 +117,26 @@ class TestRequestHandle:
 
         exec_msg = e.value.args[0]
         assert exec_msg == "Rpc run time error"
+
+
+class TestContext:
+    def test_context(self) -> None:
+        class Demo(Context):
+            bar: str
+
+        with Demo() as d:
+            d.bar = "abc"
+            assert d.bar == "abc"
+        assert not d.bar
+
+    async def test_context_in_asyncio(self) -> None:
+        class Demo(Context):
+            bar: str
+
+        with Demo() as d:
+
+            async def demo() -> None:
+                assert d.bar == "abc"
+
+            d.bar = "abc"
+            await asyncio.gather(demo(), demo())
