1d447958d9015c25c3732ef711aef617c19306d9
so1n <qaz6803609@163.com>
2021-07-12 23:16:17 +0800

    Add, Feature, rap coordinator:etcd

---
+++ rap/common/coordinator/etcd.py
@@ -0,0 +1,85 @@
+import asyncio
+import inspect
+import json
+import logging
+from typing import Any, AsyncGenerator, Callable, List, Optional
+
+from etcd3 import AioClient
+from etcd3.models import EventEventType
+
+logger: logging.Logger = logging.getLogger()
+
+
+class Etcd(object):
+    """TODO replace etcd client"""
+
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: int = 2379,
+        ttl: int = 60,
+        namespace: str = "rap",
+        cert_path: Optional[str] = None,
+        key_path: Optional[str] = None,
+        ca_path: Optional[str] = None,
+    ) -> None:
+        self.namespace: str = namespace
+        if cert_path and key_path and ca_path:
+            self._client: AioClient = AioClient(host, port, cert=(cert_path, key_path), verify=ca_path)
+        else:
+            self._client = AioClient(host, port)
+        self._ttl: int = ttl
+
+        self._lease_id: int = 0
+        self._heartbeat_future: Optional[asyncio.Future] = None
+
+    async def _heartbeat(self) -> None:
+        logger.debug(f"heartbeat by etcd, id: {self._lease_id}")
+        try:
+            await self._client.lease_keep_alive(b'{"ID":%d}\n' % self._lease_id).resp
+            await asyncio.sleep(self._ttl // 2)
+        finally:
+            await asyncio.sleep(min(5, self._ttl // 2))
+            pass
+        asyncio.ensure_future(self._heartbeat())
+
+    async def register(self, server_name: str, host: str, port: str) -> None:
+        key: str = f"{self.namespace}/{server_name}/{host}/{port}"
+        value: str = json.dumps({"host": host, "port": port})
+        if not self._heartbeat_future:
+            resp = await self._client.lease_grant(self._ttl, self._lease_id)
+            self._lease_id = resp.ID
+            self._heartbeat_future = asyncio.ensure_future(self._heartbeat())
+        logger.info(f"register key: {key} value: {value}")
+        await self._client.put(key, value, lease=self._lease_id)
+
+    async def deregister(self, server_name: str, host: str, port: str) -> None:
+        key: str = f"{self.namespace}/{server_name}/{host}/{port}"
+        await self._client.delete_range(key)
+
+    async def discovery(self, server_name: str) -> AsyncGenerator[dict, Any]:
+        key: str = f"{self.namespace}/{server_name}"
+        resp = await self._client.range(key, prefix=True)
+        for item in resp.kvs:
+            yield json.loads(item.value.decode())
+
+    async def watch(self, server_name: str, put_callback: List[Callable], del_callback: List[Callable]) -> None:
+        """etcd.AioClient not implemented api"""
+        key: str = f"{self.namespace}/{server_name}"
+        async for i in self._client.watch_create(key, prefix=True):
+            resp_dict: dict = i._data
+            if "events" in resp_dict:
+                for event in resp_dict["events"]:
+                    event_type = event.get("type", EventEventType.PUT)
+                    run_callback: Optional[List[Callable]] = []
+                    if event_type == EventEventType.PUT:
+                        run_callback = put_callback
+                    elif event_type == EventEventType.DELETE:
+                        run_callback = del_callback
+
+                    for fn in run_callback:
+                        ret = fn(event)
+                        if inspect.iscoroutine(ret):
+                            await ret
+            elif "created" in resp_dict:
+                logging.info("watch success")
