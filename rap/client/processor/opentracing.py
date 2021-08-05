de300216962a982078054b1b9d7c114e814b9408
so1n <qaz6803609@163.com>
2021-08-05 23:31:24 +0800

    Modify, Feature, client request/response support stats and refactor opentracing

--- rap/client/processor/opentracing.py
+++ rap/client/processor/opentracing.py
@@ -1,22 +1,21 @@
 from typing import Optional
 
-from jaeger_client.span_context import SpanContext  # type: ignore
-from jaeger_client.tracer import Tracer  # type: ignore
-from opentracing import InvalidCarrierException, SpanContextCorruptedException  # type: ignore
-from opentracing.ext import tags  # type: ignore
-from opentracing.propagation import Format  # type: ignore
-from opentracing.scope import Scope  # type: ignore
+from jaeger_client.span_context import SpanContext
+from jaeger_client.tracer import Tracer
+from opentracing import InvalidCarrierException, SpanContextCorruptedException
+from opentracing.ext import tags
+from opentracing.propagation import Format
+from opentracing.scope import Scope
 
 from rap.client.model import Request, Response
 from rap.common.utils import Constant
 
-from .base import BaseProcessor  # type: ignore
+from .base import BaseProcessor
 
 
 class TracingProcessor(BaseProcessor):
     def __init__(self, tracer: Tracer):
         self._tracer: Tracer = tracer
-        self._scope: Optional[Scope] = None
 
     async def process_request(self, request: Request) -> Request:
         span_ctx: Optional[SpanContext] = None
@@ -28,22 +27,23 @@ class TracingProcessor(BaseProcessor):
         except (InvalidCarrierException, SpanContextCorruptedException):
             pass
 
-        self._scope = self._tracer.start_active_span(str(request.target), child_of=span_ctx, finish_on_close=True)
-        self._scope.span.set_tag(tags.SPAN_KIND, tags.SPAN_KIND_RPC_CLIENT)
-        self._scope.span.set_tag(tags.PEER_SERVICE, request.target)
-        self._scope.span.set_tag(tags.PEER_HOSTNAME, ":".join([str(i) for i in request.header["host"]]))
-        self._scope.span.set_tag("correlation_id", request.correlation_id)
-        self._scope.span.set_tag("msg_type", request.msg_type)
+        scope: Scope = self._tracer.start_active_span(str(request.target), child_of=span_ctx, finish_on_close=True)
+        request.state.scope = scope
+        scope.span.set_tag(tags.SPAN_KIND, tags.SPAN_KIND_RPC_CLIENT)
+        scope.span.set_tag(tags.PEER_SERVICE, request.target)
+        scope.span.set_tag(tags.PEER_HOSTNAME, ":".join([str(i) for i in request.header["host"]]))
+        scope.span.set_tag("correlation_id", request.correlation_id)
+        scope.span.set_tag("msg_type", request.msg_type)
 
-        if request.msg_type is Constant.CHANNEL_REQUEST and self._scope:
-            self._scope.close()
-            self._scope = None
+        if request.msg_type is Constant.CHANNEL_REQUEST and scope:
+            scope.close()
         return request
 
     async def process_response(self, response: Response) -> Response:
-        if self._scope:
+        scope: Optional[Scope] = response.state.scope
+        if scope:
             status_code: int = response.status_code
-            self._scope.span.set_tag("status_code", status_code)
-            self._scope.span.set_tag(tags.ERROR, status_code == 200)
-            self._scope.close()
+            scope.span.set_tag("status_code", status_code)
+            scope.span.set_tag(tags.ERROR, status_code == 200)
+            scope.close()
         return response
