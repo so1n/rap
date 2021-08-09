ab6c42788803132a10f5c9447a04fa24f9f9600c
so1n <qaz6803609@163.com>
2021-08-09 23:38:20 +0800

    Modify, Refactor, Optimize call redis and add ip filter

--- rap/server/plugin/middleware/conn/ip_filter.py
+++ rap/server/plugin/middleware/conn/ip_filter.py
@@ -1,7 +1,7 @@
 import ipaddress
 from typing import TYPE_CHECKING, Dict, List, Optional, Union
 
-from aredis import StrictRedis, StrictRedisCluster  # type: ignore
+from aredis import StrictRedis, StrictRedisCluster
 
 from rap.common.conn import ServerConnection
 from rap.common.event import CloseConnEvent
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
     from rap.server.types import SERVER_EVENT_FN
 
 
-class IpBlockMiddleware(BaseConnMiddleware):
+class IpFilterMiddleware(BaseConnMiddleware):
     """
     feat:
         1. block ip
@@ -26,11 +26,11 @@ class IpBlockMiddleware(BaseConnMiddleware):
         redis: Union[StrictRedis, StrictRedisCluster],
         allow_ip_list: Optional[List[str]] = None,
         block_ip_list: Optional[List[str]] = None,
-        namespace: str = "",
+        namespace: str = "rap",
     ):
         self._redis: Union[StrictRedis, StrictRedisCluster] = redis
-        self.block_key: str = f"{self.__class__.__name__}:block_ip"
-        self.allow_key: str = f"{self.__class__.__name__}:allow_ip"
+        self.block_key: str = f"ip_filter_middleware:block_ip"
+        self.allow_key: str = f"ip_filter_middleware:allow_ip"
         self.block_cnt: int = 0
         if namespace:
             self.block_key = f"{namespace}:{self.block_key}"
@@ -48,6 +48,7 @@ class IpBlockMiddleware(BaseConnMiddleware):
 
         if self.app.window_state:
             self.app.window_state.add_priority_callback(_add_data_to_state)
+
         self.register(self._add_allow_ip)
         self.register(self._add_block_ip)
         self.register(self._remove_allow_ip)
@@ -62,7 +63,7 @@ class IpBlockMiddleware(BaseConnMiddleware):
     @staticmethod
     def ip_network_handle(ip: str) -> List[str]:
         """
-        >>> IpBlockMiddleware.ip_network_handle('192.168.0.0/31')
+        >>> IpFilterMiddleware.ip_network_handle('192.168.0.0/31')
         ['192.168.0.1', '192.168.0.2']
         """
         ip_list: List[str] = [ip]
@@ -81,13 +82,17 @@ class IpBlockMiddleware(BaseConnMiddleware):
 
     async def _add_allow_ip(self, ip: Union[str, List]) -> None:
         ip_list: List[str] = self.ip_handle(ip)
-        await self._redis.sadd(self.allow_key, ip_list[0], *ip_list[1:])
-        await self._redis.srem(self.block_key, ip_list[0], *ip_list[1:])
+        async with await self._redis.pipeline() as pipe:
+            await pipe.sadd(self.allow_key, ip_list[0], *ip_list[1:])
+            await pipe.srem(self.block_key, ip_list[0], *ip_list[1:])
+            await pipe.execute()
 
     async def _add_block_ip(self, ip: Union[str, List]) -> None:
         ip_list: List[str] = self.ip_handle(ip)
-        await self._redis.sadd(self.block_key, ip_list[0], *ip_list[1:])
-        await self._redis.srem(self.allow_key, ip_list[0], *ip_list[1:])
+        async with await self._redis.pipeline() as pipe:
+            await pipe.sadd(self.block_key, ip_list[0], *ip_list[1:])
+            await pipe.srem(self.allow_key, ip_list[0], *ip_list[1:])
+            await pipe.execute()
 
     async def _remove_allow_ip(self, ip: Union[str, List]) -> None:
         ip_list: List[str] = self.ip_handle(ip)
@@ -110,21 +115,44 @@ class IpBlockMiddleware(BaseConnMiddleware):
         return ip_list
 
     async def dispatch(self, conn: ServerConnection) -> None:
-        if conn.peer_tuple:
-            ip: str = conn.peer_tuple[0]
-            enable_allow: bool = await self._redis.scard(self.allow_key) > 0
-            if enable_allow:
-                is_allow: int = await self._redis.sismember(self.allow_key, ip)
-                if not is_allow:
-                    self.block_cnt += 1
-                    await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
-                    await conn.await_close()
-                    return
-            else:
-                is_block: int = await self._redis.sismember(self.block_key, ip)
-                if is_block:
-                    self.block_cnt += 1
-                    await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
-                    await conn.await_close()
-                    return
+        ip: str = conn.peer_tuple[0]
+        enable_allow: bool = await self._redis.scard(self.allow_key) > 0
+        if enable_allow:
+            is_allow: int = await self._redis.sismember(self.allow_key, ip)
+            if not is_allow:
+                self.block_cnt += 1
+                await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
+                await conn.await_close()
+                return
+        else:
+            is_block: int = await self._redis.sismember(self.block_key, ip)
+            if is_block:
+                self.block_cnt += 1
+                await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
+                await conn.await_close()
+                return
+        await self.call_next(conn)
+
+
+class IpBlockMiddleware(IpFilterMiddleware):
+    """Only Block Ip"""
+    async def dispatch(self, conn: ServerConnection) -> None:
+        is_block: int = await self._redis.sismember(self.block_key, conn.peer_tuple[0])
+        if is_block:
+            self.block_cnt += 1
+            await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
+            await conn.await_close()
+            return
+        await self.call_next(conn)
+
+
+class IpAllowMiddleware(IpFilterMiddleware):
+    """Only Allow Ip"""
+    async def dispatch(self, conn: ServerConnection) -> None:
+        is_allow: int = await self._redis.sismember(self.allow_key, conn.peer_tuple[0])
+        if not is_allow:
+            self.block_cnt += 1
+            await Sender(self.app, conn).send_event(CloseConnEvent("not allowed to access"))
+            await conn.await_close()
+            return
         await self.call_next(conn)
