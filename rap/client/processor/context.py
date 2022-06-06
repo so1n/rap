f80fd4e08e4cd0a7cf599228a3aa96a9568326a6
so1n <qaz6803609@163.com>
2022-06-07 00:59:09 +0800

    Modify, Refactor, change client processor context

--- rap/client/processor/context.py
+++ rap/client/processor/context.py
@@ -1,4 +1,7 @@
-from rap.client.model import Request, Response
+from types import TracebackType
+from typing import Optional, Type
+
+from rap.client.model import ClientContext, Request
 from rap.common.channel import UserChannel
 from rap.common.context import Context as _Context
 from rap.common.context import rap_context
@@ -16,17 +19,22 @@ class ContextProcessor(BaseProcessor):
     def __init__(self) -> None:
         self._context: Context = Context()
 
+    async def on_context_enter(self, context: ClientContext) -> None:
+        context.context_token = rap_context.set({})
+
+    async def on_context_exit(
+        self,
+        context: ClientContext,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        rap_context.reset(context.context_token)
+
     async def process_request(self, request: Request) -> Request:
         if request.msg_type is constant.MSG_REQUEST:
-            request.context.context_token = rap_context.set({})
             self._context.request = request
         elif request.msg_type is constant.CHANNEL_REQUEST and self._context.channel is None:
             # channel can not reset token
-            request.context.context_token = rap_context.set({})
             self._context.channel = request.context.context_channel
         return request
-
-    async def process_response(self, response: Response) -> Response:
-        if response.msg_type is constant.MSG_RESPONSE:
-            rap_context.reset(response.context.context_token)
-        return response
