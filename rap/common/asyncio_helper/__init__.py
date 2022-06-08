3af5fa5c8265680144d10bb4f8971b7c43f6c61d
so1n <qaz6803609@163.com>
2022-06-08 19:31:58 +0800

    Add, Feature, add taskgroup from python3.4.10rc

--- rap/common/asyncio_helper/__init__.py
+++ rap/common/asyncio_helper/__init__.py
@@ -1,6 +1,7 @@
 from .deadline import Deadline, IgnoreDeadlineTimeoutExc, deadline_context, get_deadline
 from .semaphore import Semaphore
 from .set_event import SetEvent
+from .taskgroups import TaskGroup, TaskGroupExc
 from .util import (
     as_first_completed,
     can_cancel_sleep,
